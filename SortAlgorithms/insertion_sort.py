d1d657bb52a163c7390c781bf778c167778831ee
Michael Gilliland <mjg.py3@gmail.com>
2013-01-03 18:10:08 -0500

    Added an insertion sort coded in Python

---
+++ SortAlgorithms/insertion_sort.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Created by Michael Gilliland
+# Date: Thu Jan  3 18:02:47 EST 2013
+# 
+# 
+
+def insertion_sort(list):
+    for i in xrange(1, len(list)):
+        k = i
+        while k > -1 and list[k] < list[k-1]:
+            list[k], list[k-1] = list[k-1], list[k]
+            k -= 1
+
+if __name__ == '__main__':
+    sort_me = [1, 43, 123, 5, 2, 654, 23, 4, 123]
+    insertion_sort(sort_me)
+    print sort_me
+
+    sort_me = [134, 21, "Michael", 24, 1, "Foobar", 'foobar', "Django Unchained", 23, 12, 1, "j"]
+    insertion_sort(sort_me)
+    print sort_me
