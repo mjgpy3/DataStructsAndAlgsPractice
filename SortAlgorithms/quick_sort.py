916d1faa33ca42a5206ae1c0ed0865566fee1fb2
Michael Gilliland <mjg.py3@gmail.com>
2013-01-03 19:37:46 -0500

    Added a quick sort in Python

---
+++ SortAlgorithms/quick_sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Created by Michael Gilliland
+# Date: Thu Jan  3 19:30:22 EST 2013
+# 
+# 
+
+from random import choice
+
+def quick_sort(list):
+    if len(list) < 2:
+        return list
+
+    greater = []
+    less = []
+    pivot = list.pop()
+    
+
+    for elem in list:
+        if elem < pivot:
+            less.append(elem)
+        else:
+            greater.append(elem)
+
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+if __name__ == '__main__':
+    a = [1, 324, 12, 3, 12, 344, 12, 54, 1, 345, 12, 64, 872, 3425, 2]
+
+    print quick_sort(a)
