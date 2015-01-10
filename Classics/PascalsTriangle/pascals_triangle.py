efb4bd41ea5c66ac13bf0ef8e6193164a138be2c
Michael Gilliland <mjg.py3@gmail.com>
2015-01-10 15:56:21 -0500

    improvements to Pascal's triangle

--- Classics/PascalsTriangle/pascals_triangle.py
+++ Classics/PascalsTriangle/pascals_triangle.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-def build(depth, last_layer=[]):
-    if last_layer == []:
+def item_at(i, last_layer):
+    return (
+        (last_layer[i-1] if i > 0 else 0) +
+        (last_layer[i] if i < len(last_layer) else 0)
+    )
+
+def build(depth, last_layer=None):
+    if last_layer is None:
         return [[1]] + build(depth-1, [1])
-    if depth < 1:
+    if depth == 0:
         return []
 
-    layer = []
-    for i in xrange(len(last_layer)+1):
-        item = 0
-        if i-1 >= 0:
-            item += last_layer[i-1]
-        if i < len(last_layer):
-            item += last_layer[i]
-        layer.append(item)
+    layer = [item_at(i, last_layer) for i in xrange(len(last_layer)+1)]
 
     return [layer] + build(depth-1, layer)
