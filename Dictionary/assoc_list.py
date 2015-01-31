d07d664fccf310f9cecd34a7b710c8fe1ab53405
Michael Gilliland <mjg.py3@gmail.com>
2015-01-31 12:40:40 -0500

    Added stubs for each impl method

--- Dictionary/assoc_list.py
+++ Dictionary/assoc_list.py
@@ -3,3 +3,12 @@ from dictionary import Dictionary
 class AssocList(Dictionary):
     def insert(self, key, value):
         pass
+
+    def delete(self, key):
+        pass
+
+    def get(self, key):
+        pass
+
+    def __contains__(self, key):
+        pass
--- Dictionary/test_assoc_list.py
+++ Dictionary/test_assoc_list.py
@@ -10,5 +10,14 @@ class AssocListTests(unittest.TestCase):
     def test_insert_can_be_used(self):
         self.uut.insert('answer', 42)
 
+    def test_delete_can_be_used(self):
+        self.uut.delete('answer')
+
+    def test_get_can_be_used(self):
+        self.uut.get('answer')
+
+    def test_contains_can_be_used(self):
+        'answer' in self.uut
+
 if __name__ == '__main__':
     unittest.main()
