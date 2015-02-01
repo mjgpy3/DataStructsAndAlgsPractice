c73b87b5cce92d47fdecfab9fc1c054df31e4f72
Michael Gilliland <mjg.py3@gmail.com>
2015-01-31 19:20:17 -0500

    Started on load factor portion

--- Dictionary/hash_map.py
+++ Dictionary/hash_map.py
@@ -1,4 +1,12 @@
 from dictionary import Dictionary
 
 class HashMap(Dictionary):
-    pass
+    def __init__(self, load_factor = 0.75):
+        self.__load_factor = load_factor
+        self.__capacity = 16
+
+    def capacity(self):
+        return self.__capacity
+
+    def doubling_size(self):
+        return self.__capacity * self.__load_factor
--- Dictionary/test_hash_map.py
+++ Dictionary/test_hash_map.py
@@ -8,5 +8,15 @@ class AssocListTests(unittest.TestCase):#, DictTestCases):
     def setUp(self):
         self.uut = HashMap()
 
+    def test_initial_current_capacity_is_16(self):
+        self.assertEqual(16, self.uut.capacity())
+
+    def test_initial_doubling_size_is_12(self):
+        self.assertEqual(12, self.uut.doubling_size())
+
+    def test_when_inialized_with_one_half_then_doubling_size_is_8(self):
+        uut = HashMap(0.5)
+        self.assertEqual(8, uut.doubling_size())
+
 if __name__ == '__main__':
     unittest.main()
